fc3b5ce63167f5357bf1691b2530a1040fd5857b
RetriveAgustin <retrive.agustin@gmail.com>
2023-05-10 02:13:21 -0300

    logo fixed

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,35 +3,28 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-
   const navigate = useNavigate();
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand>
+          <div className="logo-container">
             <img
               src="../../assets/logopng.png"
               alt="logo"
               className="navbar-logo"
             />
-            <span> | Fuerzas Privadas Latinas</span>
+            <span>║ Fuerzas Privadas Latinas</span>
+          </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link onClick={()=> navigate("/about")}>
-                About
-            </Nav.Link>
-            <Nav.Link>
-                Condor
-            </Nav.Link>
-            <Nav.Link>
-                Delta
-            </Nav.Link>
-            <Nav.Link>
-                Mike
-            </Nav.Link>
+            <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
+            <Nav.Link>Condor</Nav.Link>
+            <Nav.Link>Delta</Nav.Link>
+            <Nav.Link>Mike</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

fe50a971eecdeca47338027d38e5bf934506263b
RetriveAgustin <retrive.agustin@gmail.com>
2023-05-10 01:54:31 -0300

    buttons fixed

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,67 +1,36 @@
 import "./NavBar.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand>
-          <NavLink to="/" className="navbar-brand">
             <img
               src="../../assets/logopng.png"
               alt="logo"
               className="navbar-logo"
             />
             <span> | Fuerzas Privadas Latinas</span>
-          </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link>
-              <NavLink
-                to="/about"
-                style={{
-                  textDecoration: "none",
-                  color: "#A0A0A0",
-                }}
-              >
+            <Nav.Link onClick={()=> navigate("/about")}>
                 About
-              </NavLink>
             </Nav.Link>
             <Nav.Link>
-              <NavLink
-                to="/condor"
-                style={{
-                  textDecoration: "none",
-                  color: "#A0A0A0",
-                }}
-              >
                 Condor
-              </NavLink>
             </Nav.Link>
             <Nav.Link>
-              <NavLink
-                to="/delta"
-                style={{
-                  textDecoration: "none",
-                  color: "#A0A0A0",
-                }}
-              >
                 Delta
-              </NavLink>
             </Nav.Link>
             <Nav.Link>
-              <NavLink
-                to="/mike"
-                style={{
-                  textDecoration: "none",
-                  color: "#A0A0A0",
-                }}
-              >
                 Mike
-              </NavLink>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>

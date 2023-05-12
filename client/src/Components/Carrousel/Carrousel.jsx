56dcb12c9e0f26038d499dfdba46aca1ed5fdd45
RetriveAgustin <retrive.agustin@gmail.com>
2023-05-12 13:34:44 -0300

    carousel estable

--- client/src/Components/Carrousel/Carrousel.jsx
+++ client/src/Components/Carrousel/Carrousel.jsx
@@ -1,9 +1,29 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import "./Carrousel.css";
 
-const Carrousel = () => {
+/* IMPORT COMPONENTS */
+import { Carousel } from "react-bootstrap";
+
+const Carrousel = (props) => {
 
   return (
-    <h2>Carousel</h2>
+    <Carousel>
+      {
+        props.fotos.map(foto => {
+          return (
+            <Carousel.Item>
+              <img className="d-block carousel-img" src={foto.image} alt={foto.alt_text} />
+
+              <Carousel.Caption>
+                <h3>{foto.label}</h3>
+                <p>{foto.text}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        })
+      }
+    </Carousel>
   );
 };
 
--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,12 +1,36 @@
 import "./Home.css"
 /* IMPORT COMPONENTES */
 import Carrousel from "../../Components/Carrousel/Carrousel";
+import fplcard from "./assets/fplcard.png";
+import image1 from "./assets/1.jpg";
+import image2 from "./assets/2.jpg";
 
 
 const Home = () => {
+  let images = [
+    {
+      image: fplcard,
+      alt_text: "first slice",
+      label: "",
+      text: ""
+    },
+    {
+      image: image1,
+      alt_text: "second slice",
+      label: "Delta",
+      text: "Honor | Lealtad | Disciplina"
+    },
+    {
+      image: image2,
+      alt_text: "tird slice",
+      label: "Label",
+      text: "some text"
+    }
+  ]
+
   return (
     <div className="home-container">
-      <Carrousel />
+      <Carrousel fotos={images}/>
     </div>
   )
 }
